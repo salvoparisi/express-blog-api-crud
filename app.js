2e41969cc2be7c39b8d30a75e1c1be9417d57699
salvoparisi <psalvo065@gmail.com>
2024-11-05 15:49:52 +0100

    add controllers/store

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const postController = require('./controllers/posts.js')
 const PORT = 3000
 
+app.use(express.json())
+
 app.listen(PORT, ()=>{
     console.log(`Server listening http://localhost:${PORT}`);
 })
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,11 @@
 const posts = require('../db.js')
 
 const store = (req, res) => {
-    
+    const { title, slug, content } = req.body;
+
+    const newPost = { title, slug, content };
+    posts.push(newPost);
+    res.json(posts);
 }
 
 module.exports = { store }
