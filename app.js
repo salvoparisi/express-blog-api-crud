da851e63c81ec1c9350bdf575f5907cc8d221d43
salvoparisi <psalvo065@gmail.com>
2024-11-07 14:33:37 +0100

    add notFound

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const postController = require('./controllers/posts.js')
+const notFoundMiddleware = require('./middlewares/notFoundMiddleware.js')
 const PORT = 3000
 
 app.use(express.json())
+
+app.use(notFoundMiddleware);
 
 app.listen(PORT, ()=>{
     console.log(`Server listening http://localhost:${PORT}`);
