7de3452645916344a6992348b4a68f4293d146c6
salvoparisi <psalvo065@gmail.com>
2024-11-07 15:05:44 +0100

    add logger

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ const express = require('express')
 const app = express()
 const postController = require('./controllers/posts.js')
 const notFoundMiddleware = require('./middlewares/notFoundMiddleware.js')
+const loggerMiddleware = require('./middlewares/loggerMiddleware.js')
 const PORT = 3000
 
 app.use(express.json())
+
+app.use('/', loggerMiddleware)
+/*
+app.use('/', (req, res, next) => {
+    throw new Error("Hai spaccato tutto!");
+  });
+*/
+app.post('/', postController.store)
+app.put('/:id', postController.update)
+app.delete('/:id', postController.destroy)
 
 app.use(notFoundMiddleware);
 
 app.listen(PORT, ()=>{
     console.log(`Server listening http://localhost:${PORT}`);
 })
-
-app.post('/', postController.store)
-
-app.put('/:id', postController.update)
-
-app.delete('/:id', postController.destroy)
