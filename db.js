04b048c7493c35b4d4c5be717a90081e1665cbf4
salvoparisi <psalvo065@gmail.com>
2024-11-20 14:27:43 +0100

    add array

--- db.js
+++ db.js
@@ -1,26 +1,34 @@
 [
     {
         "id": "1",
-        "nome": "Luna",
-        "eta": 3,
-        "colore": "grigio"
+        "image": "https://placehold.co/600x400",
+        "title": "Esplorando le Montagne",
+        "description": "Un viaggio attraverso le affascinanti catene montuose con viste mozzafiato e avventure indimenticabili.",
+        "category": "Viaggi",
+        "tags": ["Avventura", "Natura", "Escursionismo"]
+    },
+    {
+        "id": "2",
+        "image": "https://placehold.co/600x400",
+        "title": "Ricette Facili e Veloci",
+        "description": "Scopri ricette semplici per preparare piatti deliziosi in pochi minuti.",
+        "category": "Cucina",
+        "tags": ["Ricette", "Facile", "Veloce"]
     },
     {
         "id": "3",
-        "nome": "Oliver",
-        "eta": 4,
-        "colore": "arancione"
+        "image": "https://placehold.co/600x400",
+        "title": "Guida alla Programmazione",
+        "description": "Una guida completa per imparare le basi della programmazione e sviluppare applicazioni.",
+        "category": "Tecnologia",
+        "tags": ["Codice", "Sviluppo", "Programmazione"]
     },
     {
         "id": "4",
-        "nome": "Nala",
-        "eta": 1,
-        "colore": "marrone tigrato"
-    },
-    {
-        "id": "5",
-        "nome": "Simba",
-        "eta": 5,
-        "colore": "nero"
+        "image": "https://placehold.co/600x400",
+        "title": "Migliora il tuo Fitness",
+        "description": "Consigli utili e programmi di allenamento per raggiungere i tuoi obiettivi di fitness.",
+        "category": "Salute",
+        "tags": ["Fitness", "Allenamento", "Salute"]
     }
 ]
